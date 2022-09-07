ea8c1235f3ca0cc6a19438a15964519694846483
Jasper <450213487@qq.com>
2022-09-07 23:23:59 +0800

    add loadModel

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "model.h"
 #include "tgaimage.h"
 #include "render_helper.h"
 const TGAColor white = TGAColor(255, 255, 255, 255);
@@ -6,9 +7,25 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 
 int main(void)
 {
-	TGAImage image(1080, 720, TGAImage::RGB);
+	auto width = 1024;
+	auto height = 768;
+	TGAImage image(width, height, TGAImage::RGB);
+	Model model("obj/african_head/african_head.obj");
+	for(int i = 0; i < model.nfaces();i++)
+	{
+		for(int j = 0 ; j < 3; j++)
+		{
+			auto v0 = model.vert(i, j);
+			auto v1 = model.vert(i, (j + 1)%3);
+			int x0 = (v0.x + 1.0) * width / 2;
+			int y0 = (v0.y + 1.0) * height / 2;
+			int x1 = (v1.x + 1.0) * width / 2;
+			int y1 = (v1.y + 1.0) * height / 2;
+			line(x0, y0, x1, y1, image, white);
+		}
+	}
 	//line(702, 666, 10, 20, image,white);
-	line2(555, 375, 15, 15, image, red);
+	//line2(555, 375, 15, 15, image, red);
 	//line(555, 375, 15, 15, image, white);
 	//image.flip_vertically();
 	image.write_tga_file("output.tga");
---
+++ model.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#include "geometry.h"
+
+class Model
+{
+    std::vector<vec3> verts{};     // array of vertices
+    std::vector<vec2> tex_coord{}; // per-vertex array of tex coords
+    std::vector<vec3> norms{};     // per-vertex array of normal vectors
+    std::vector<int> facet_vrt{};
+    std::vector<int> facet_tex{};  // per-triangle indices in the above arrays
+    std::vector<int> facet_nrm{};
+public:
+	Model(const std::string fileName);
+    int nverts() const;
+    int nfaces() const;
+    vec3 vert(const int i) const;
+    vec3 vert(const int iface, const int nthvert) const;
+};
