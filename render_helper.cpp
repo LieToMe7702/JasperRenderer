4abfdee99f234aefba826f7c47b1611173267b4f
Jasper <450213487@qq.com>
2022-09-22 22:05:36 +0800

    modify barycentric

--- render_helper.cpp
+++ render_helper.cpp
@@ -71,11 +71,11 @@ vec3 barycentric(const std::vector<vec2>& points, const vec2& p)
 
 vec3 barycentric(const std::vector<vec3>& points, const vec2& p)
 {
-	vec3 vec_x(points[2][0] - points[0][0], points[1][0] - points[0][0], points[0][0] - p[0]);
-	vec3 vec_y(points[2][1] - points[0][1], points[1][1] - points[0][1], points[0][1] - p[1]);
+	vec3 vec_x(points[1][0] - points[0][0], points[2][0] - points[0][0], points[0][0] - p[0]);
+	vec3 vec_y(points[1][1] - points[0][1], points[2][1] - points[0][1], points[0][1] - p[1]);
 	vec3 cross_vec = cross(vec_x, vec_y);
 	if (std::abs(cross_vec.z) < 1e-3) return { -1, 1, 1 };
-	vec3 res{ 1.0f - (cross_vec.x + cross_vec.y) / cross_vec.z,cross_vec.y / cross_vec.z,cross_vec.x / cross_vec.z };
+	vec3 res{ 1.0f - (cross_vec.x + cross_vec.y) / cross_vec.z,cross_vec.x / cross_vec.z,cross_vec.y / cross_vec.z };
 	return res;
 }
 
